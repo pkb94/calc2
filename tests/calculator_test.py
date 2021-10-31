b845bed969e7c64208ffe30316e497fb31a2316e
Madhurimasingh1 <67088283+Madhurimasingh1@users.noreply.github.com>
2021-10-31 18:43:36 -0400

    update

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -3,7 +3,7 @@ from calculator.calculator import Calculator
 
 
 def test_calculator_add():
-    """Testing the Add function of the calculator"""
+    """Testing the Add function of the calculator """
     assert  Calculator.add_number(1,2)==3
 
 
