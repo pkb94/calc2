1096e9c503ab55ab2ffe40d774b6a97ef50dbb4d
Pooja Kittanakere Balaji <pk73@njit.edu>
2021-11-02 09:45:02 -0400

    Updated the divide and division by zero exception unittest

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,32 +1,30 @@
 """Testing the Calculator"""
-from calculator.main import Calculator
+import unittest
+from calculator.calculator import Calculator
 
-def test_calculator_result():
-    """testing calculator result is 0"""
-    calc = Calculator()
-    assert calc.result == 0
 
 def test_calculator_add():
-    """Testing the Add function of the calculator"""
-    #Arrange by instantiating the calc class
-    calc = Calculator()
-    #Act by calling the method to be tested
-    calc.add_number(4)
-    #Assert that the results are correct
-    assert calc.result == 4
+    """Testing the Add function of the calculator """
+    assert  Calculator.add_number(1,2)==3
 
-def test_calculator_get_result():
-    """Testing the Get result method of the calculator"""
-    calc = Calculator()
-    assert calc.get_result() == 0
 
 def test_calculator_subtract():
     """Testing the subtract method of the calculator"""
-    calc = Calculator()
-    calc.subtract_number(1)
-    assert calc.get_result() == -1
+    assert  Calculator.subtract_number(1,2)== -1
+
 def test_calculator_multiply():
-    """ tests multiplication of two numbers"""
-    calc = Calculator()
-    result  = calc.multiply_numbers(1,2)
-    assert result == 2
+    """Testing the multiply method of the calculator"""
+    assert Calculator.multiply_numbers(2,3)== 6
+
+def test_calculator_divide():
+    """Testing the divide method of the calculator"""
+    assert Calculator.divide_numbers(4,2)== 2
+
+class MyTestCase(unittest.TestCase):
+    """A test case is the individual unit of testing.unittest provides a base class"""
+    def test_calculator_divide_by_zero(self):
+        """Testing the divide method of the calculator when dividing by zero"""
+        with self.assertRaises(ZeroDivisionError):
+            Calculator.divide_numbers(4,0)
+if __name__ == '__main__':
+    unittest.main()
