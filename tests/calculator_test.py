6f8bbef8b634b0170fa11fd6addb70c399377558
Pooja Kittanakere Balaji <pk73@njit.edu>
2021-11-04 20:29:42 -0400

    Updated the test file by adding the static methods

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,49 +1,73 @@
 """Testing the Calculator"""
+import pprint
 import unittest
-from calculator.main import Calculator
 
-def test_calculator_result():
-    """testing calculator result is 0"""
-    calc = Calculator()
-    assert calc.result == 0
+import pytest
 
-def test_calculator_add():
+from calculator.calculator import Calculator
+
+# pylint: disable=unused-argument,redefined-outer-name,missing-function-docstring
+@pytest.fixture
+def clear_history():
+    Calculator.clear_history()
+
+def test_calculator_add(clear_history):
     """Testing the Add function of the calculator"""
-    #Arrange by instantiating the calc class
-    calc = Calculator()
-    #Act by calling the method to be tested
-    calc.add_number(4)
-    #Assert that the results are correct
-    assert calc.result == 4
+    assert Calculator.add_number(1,2) == 3
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.add_number(4, 2) == 6
+    assert Calculator.history_count() == 4
+    assert Calculator.get_result_of_last_calculation_added_to_history() == 6
+    pprint.pprint(Calculator.history)
 
-def test_calculator_get_result():
-    """Testing the Get result method of the calculator"""
-    calc = Calculator()
-    assert calc.get_result() == 0
+def test_clear_history(clear_history):
+    assert Calculator.add_number(1,2) == 3
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.add_number(4, 2) == 6
+    assert Calculator.history_count() == 4
+    assert Calculator.clear_history() is True
+    assert Calculator.history_count() == 0
 
-def test_calculator_subtract():
+def test_count_history(clear_history):
+    assert Calculator.history_count() == 0
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.multiply_numbers(2,5) == 10
+    assert Calculator.division_numbers(10,2) == 5
+    assert Calculator.subtract_number(10,10) ==0
+    assert Calculator.history_count() == 5
+
+def test_get_last_calculation_result(clear_history):
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.multiply_numbers(-1,5) == -5
+    assert Calculator.get_result_of_last_calculation_added_to_history() == -5
+
+def test_get_first_calculation_result(clear_history):
+    assert Calculator.add_number(2, 2) == 4
+    assert Calculator.add_number(3, 2) == 5
+    assert Calculator.multiply_numbers(15, 5) == 75
+    assert Calculator.get_result_of_first_calculation_added_to_history() == 4
+
+def test_calculator_subtract(clear_history):
     """Testing the subtract method of the calculator"""
-    calc = Calculator()
-    calc.subtract_number(1)
-    assert calc.get_result() == -1
-def test_calculator_multiply():
-    """ tests multiplication of two numbers"""
-    calc = Calculator()
-    result  = calc.multiply_numbers(2,2)
-    assert result == 4
+    assert Calculator.subtract_number(8, 2) == 6
 
-def test_calculator_division():
+def test_calculator_multiply(clear_history):
     """ tests multiplication of two numbers"""
-    calc = Calculator()
-    result = calc.division_numbers(2,2)
-    assert result == 1
+    assert Calculator.multiply_numbers(4,2) == 8
+
+def test_calculator_divide():
+    """Testing the divide method of the calculator"""
+    assert Calculator.division_numbers(10,2)== 5
 
 class MyTestCase(unittest.TestCase):
     """A test case is the individual unit of testing.unittest provides a base class"""
     def test_calculator_divide_by_zero(self):
         """Testing the divide method of the calculator when dividing by zero"""
         with self.assertRaises(ZeroDivisionError):
-            calc = Calculator()
-            calc.division_numbers(2,0)
+            Calculator.division_numbers(4,0)
 if __name__ == '__main__':
     unittest.main()
