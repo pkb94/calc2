b44e32f96aa6622c346668bd575657c62568687d
Pooja Kittanakere Balaji <pk73@njit.edu>
2021-11-09 19:53:53 -0500

    Refactored calculator class

--- calc/calculator.py
+++ calc/calculator.py
@@ -1,16 +1,28 @@
 """ This is the increment function"""
-from calc.operations.addition import Addition
-from calc.operations.subtraction import Subtraction
+from calc.history.calculations import Calculations
 
+#the calculator class just contains the methods to calculate
 class Calculator:
     """ This is the Calculator class"""
-
+    #the calculator class just calls methods on Calculations class
     @staticmethod
-    def add_numbers(value_a, value_b):
-        """ adds two numbers"""
-        return Addition.add(value_a, value_b)
-
+    def get_result_value():
+        """ This is the gets the result of the calculation"""
+        # I made this method so that I don't have more than one action per function
+        return Calculations.get_last_calculation_result_value()
     @staticmethod
-    def subtract_numbers(value_a, value_b):
-        """ subtract number from result"""
-        return Subtraction.subtract(value_a,value_b)
+    #tuple allows me to pass in as many values as a I want
+    def add_numbers(tuple_values: tuple):
+        """ adds list of numbers"""
+        Calculations.add_addition_calculation(tuple_values)
+        return True
+    @staticmethod
+    def subtract_numbers(tuple_values: tuple):
+        """ subtract a list of numbers from result"""
+        Calculations.add_subtraction_calculation(tuple_values)
+        return True
+    @staticmethod
+    def multiply_numbers(tuple_values: tuple):
+        """ multiplication number from result"""
+        Calculations.add_multiplication_calculation(tuple_values)
+        return True
--- calc/operations/subtraction.py
+++
@@ -1,7 +0,0 @@
-"""Subtraction Class"""
-
-class Subtraction:
-
-    @staticmethod
-    def subtract(value_a, value_b):
-        return value_a - value_b
