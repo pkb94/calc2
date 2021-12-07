3366e6522feb10a0a79b5506feeaeaaddb4eb36f
Madhurimasingh1 <67088283+Madhurimasingh1@users.noreply.github.com>
2021-12-07 18:35:07 -0500

    write

--- app/controllers/index_controller.py
+++ app/controllers/index_controller.py
@@ -1,28 +1,7 @@
 from app.controllers.controller import ControllerBase
-from calc.calculator import Calculator
-from flask import render_template, request, flash, redirect, url_for
+from flask import render_template
 
-
-class CalculatorController(ControllerBase):
-    @staticmethod
-    def post():
-        if request.form['value1'] == '' or request.form['value2'] == '':
-            error = 'You must enter a value for value 1 and or value 2'
-        else:
-            flash('You successfully calculated')
-            flash('You are awesome')
-
-            # get the values out of the form
-            value1 = request.form['value1']
-            value2 = request.form['value2']
-            operation = request.form['operation']
-            # make the tuple
-            my_tuple = (value1, value2)
-            # this will call the correct operation
-            getattr(Calculator, operation)(my_tuple)
-            result = str(Calculator.get_last_result_value())
-            return render_template('result.html', value1=value1, value2=value2, operation=operation, result=result)
-        return render_template('calculator2.html', error=error)
+class IndexController(ControllerBase):
     @staticmethod
     def get():
-        return render_template('calculator2.html')
+        return render_template('index.html')
