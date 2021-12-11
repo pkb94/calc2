a572d6c8ca30e940d5154da59c35882cf9bd5e26
Pooja Kittanakere Balaji <pk73@njit.edu>
2021-12-11 16:13:07 -0500

    files for Project3

--- calc/calculator.py
+++ calc/calculator.py
@@ -26,3 +26,15 @@ class Calculator:
         """ multiplication number from result"""
         Calculations.add_multiplication_calculation_to_history(tuple_values)
         return True
+    @staticmethod
+    def getHistory():
+        """ Get history """
+        return Calculations.history
+    @staticmethod
+    def getHistoryFromCSV():
+        """ Get history """
+        return Calculations.readHistoryFromCSV()
+    @staticmethod
+    def writeHistoryToCSV():
+        """ Get history """
+        return Calculations.writeHistoryToCSV()
